02f128e7361064b53cc647e11d81f38a5f5276c9
thecodeTony <tonyptk3@gmail.com>
2022-10-18 18:44:07 -0700

    addition of integer task done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - add two numbers
+ * @a: the first number
+ * @c: the second number
+ * Return: the addition of both
+ */
+int add(int a, int c)
+{
+	return (a + b);
+}
